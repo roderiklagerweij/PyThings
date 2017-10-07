64c52859f5fdeda36598761f8a744e23e790445d
= <=>
2017-10-07 15:27:03 +0200

    Export as jpg

--- exporter.py
+++ exporter.py
@@ -4,5 +4,5 @@ import pygame
 
 def export(image, module, export_counter):
     name = '_'.join([module.get_name(), 'v' + module.get_version(), str(export_counter)])
-    pygame.image.save(image, './images/' + name + '.png')
+    pygame.image.save(image, './images/' + name + '.jpg')
     return name
