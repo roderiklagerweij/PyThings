1f565dd1a9431ff18de0b3d260d32e0d84874cb6
= <=>
2017-10-25 14:26:33 +0200

    More cleanup

--- util/constraint_applyer.py
+++ util/constraint_applyer.py
@@ -11,26 +11,9 @@ def apply_width_constraint(parent, id_list):
         views.extend(view_finder.find_views_with_id(id, parent))
 
     for i in range(10):
-
-        # for view in views:
-        #     if view.debug_id == 'container':
-        #         print ('begin', view.width)
-
         parent.measure()
-        # for view in views:
-        #     if view.debug_id == 'container':
-        #         print ('after measure', view.width)
-
         parent.post_measure(settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT)
-        # for view in views:
-        #     if view.debug_id == 'container':
-        #         print ('after post measure', view.width)
-
         parent.apply_gravity(0, 0, settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT)
-        # for view in views:
-        #     if view.debug_id == 'container':
-        #         print ('after apply gravity', view.width)
-
 
         max_width = 0
         for view in views:
@@ -38,12 +21,7 @@ def apply_width_constraint(parent, id_list):
                 max_width = view.width
 
         for view in views:
-            # print (view.width, 'set to', max_width)
             view.width = max_width
-
-            # if view.debug_id == 'container':
-            #     print (view.width)
-
 
 def apply_height_constraint(parent, id_list):
 
